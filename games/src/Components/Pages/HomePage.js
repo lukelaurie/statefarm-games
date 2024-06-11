1dac5727419b088c5940d4ea276b2e354dcfd012
lukelaurie <lukelaurie13@gmail.com>
2024-06-11 11:28:46 -0500

    links

--- games/src/Components/Pages/HomePage.js
+++ games/src/Components/Pages/HomePage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
       <h1>The home page</h1>
+      <nav>
+        <ul>
+          <li><Link to="/car">Go to Car Page</Link></li>
+          <li><Link to="/trivia">Go to Trivia Page</Link></li>
+        </ul>
+      </nav>
     </>
   );
 };
