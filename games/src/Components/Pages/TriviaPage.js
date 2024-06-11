ea0cbb1abb864067db250ca8cb7de703b2b848ea
Neer Jain <neerjain04@gmail.com>
2024-06-11 18:32:03 -0500

    Fixed merge conflicts

--- games/src/Components/Pages/TriviaPage.js
+++ games/src/Components/Pages/TriviaPage.js
@@ -1,28 +1,16 @@
 import React from "react";
-<<<<<<< HEAD
-import TriviaHeader from "../../TriviaPage/TrivialHeader";
-import TextBox from "../../TriviaPage/TextBox";
+import JakeSpeaking from "../../TriviaPage/JakeSpeaking";
 import AnimationComponent from "../../TriviaPage/AnimationComponent";
-=======
-import TriviaHeader from "../TriviaPage/TrivialHeader";
-import JakeSpeaking from "../TriviaPage/JakeSpeaking";
->>>>>>> 3246f8bc6518bf8844b699fd6d84cc1a298a12d6
 
 const TriviaPage = () => {
   return (
     <>
-<<<<<<< HEAD
-      <h1>The trivia page hi</h1>
-      <TriviaHeader/>
-      <TextBox/>
-      <AnimationComponent/>
-=======
     <font color='red'>
       <h6>State Farm Trivia!</h6>
       <h5>with Jake and Caitlin</h5>
       </font>
       <JakeSpeaking/>
->>>>>>> 3246f8bc6518bf8844b699fd6d84cc1a298a12d6
+      <AnimationComponent/>
     </>
   );
 };
