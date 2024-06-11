e88c2d8ec3196eb49885bab2af52040119e7dda7
jklavallee <jade6@illinois.edu>
2024-06-11 12:57:14 -0500

    Added 2 new files

---
+++ games/src/Components/TriviaPage/TrivialHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TriviaHeader = () => {
+  return (
+    <>
+      <h1>This is a header</h1>
+      <h2> Basketball trivial page!! </h2>
+    </>
+  );
+};
+
+export default TriviaHeader;
