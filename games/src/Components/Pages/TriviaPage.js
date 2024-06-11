f25df1310b69063e336da4da28696c5f3ef2c577
jklavallee <jade6@illinois.edu>
2024-06-11 12:50:09 -0500

    Added basic button for pop-up text question box

--- games/src/Components/Pages/TriviaPage.js
+++ games/src/Components/Pages/TriviaPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import TriviaHeader from "../TriviaPage/TrivialHeader";
+import TextBox from "../TriviaPage/TextBox";
 
 const TriviaPage = () => {
   return (
     <>
-      <h1>The trivia page</h1>
+      <h1>The trivia page hi</h1>
+      <TriviaHeader/>
+      <TextBox/>
     </>
   );
 };
