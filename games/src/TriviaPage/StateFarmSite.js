9bdf15aca0756e5c9dfe844e7b14f394d0d60edb
jklavallee <jade6@illinois.edu>
2024-06-11 21:22:42 -0500

    Added back button and updated Jake's text box

--- games/src/TriviaPage/StateFarmSite.js
+++ games/src/TriviaPage/StateFarmSite.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './Button.css';
+import { Link } from "react-router-dom";
 
+<Link to="/">Back</Link>
 function StateFarmSite() {
   
-    const goToHomePage = () => {
+    const goToSFSite = () => {
       window.location.href = 'https://www.statefarm.com';
+    };
+
+    const goToHomePage = () => {
+      window.location.href = '/';
     };
   
     return (
       <div>
         {/* Button to navigate to the home page */}
-        <button onClick={goToHomePage} className="website-button">
-          <p>
+        <button onClick={goToSFSite} className="SFwebsite-button">
+          <p1>
           Visit State Farm Homepage to Search for Trivia Solutions
-          </p>
+          </p1>
+          </button>
+          <button onClick={goToHomePage} className="back-button">
+          <p1>
+          Back
+          </p1>
           </button>
       </div>
     );
