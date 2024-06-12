28afcb4ed394db28fb7b3409df6b64a9caca5375
Neer Jain <neerjain04@gmail.com>
2024-06-11 21:22:44 -0500

    Fixed background background

--- games/src/Components/Pages/TriviaPage.js
+++ games/src/Components/Pages/TriviaPage.js
@@ -3,15 +3,16 @@ import JakeSpeaking from "../../TriviaPage/JakeSpeaking";
 import AnimationComponent from "../../TriviaPage/AnimationComponent";
 import TriviaHeader from '../../TriviaPage/TriviaHeader';
 import StateFarmSite from '../../TriviaPage/StateFarmSite'
+import '../../App.css';
 
 const TriviaPage = () => {
   return (
-    <>
+    <div className="TriviaBackground">
       <TriviaHeader/>
       <StateFarmSite/>
       <JakeSpeaking/>
       <AnimationComponent/>
-    </>
+    </div>
   );
 };
 
